00758d3fa4d6947cf712acef799f030a2e2bdb93
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-02-04 00:53:35 +0900

    update:sessionをredisで保持

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # session
+  config.session_store :redis_store, servers: 'redis://localhost:6379/0/session', expire_in: 30.minutes
 end
