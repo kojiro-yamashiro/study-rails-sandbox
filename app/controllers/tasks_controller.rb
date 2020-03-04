2044e02f5c3d5ada0276e8354ac7f32589ae84ee
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-03-04 22:49:41 +0900

    フォームを受け付けるcreateメソッドを用意した。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+
+
   def index
   end
 
@@ -10,5 +12,22 @@ class TasksController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
