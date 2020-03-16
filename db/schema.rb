3d857677247a319353c3404580467ee1b71bb942
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-03-16 22:29:04 +0900

    delete users.schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,4 @@ ActiveRecord::Schema.define(version: 2020_03_02_153259) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "age"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
