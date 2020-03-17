c8a4254363609aa4b39759dfd020f7db618fed01
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-03-18 00:14:13 +0900

    feat dream controllerを実装

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,7 +1,27 @@
 class DreamsController < ApplicationController
 
+  def index
+    @dreams = Dream.all
+  end
+
   def new
     @dream = Dream.new
+  end
+
+  def create
+    dream = Dream.new(dream_params)
+    dream.save
+    redirect_to dreams_url, notice: '夢日記に登録しました。'
+  end
+
+  def show
+    @dream = Dream.where(id: params[:id])
+  end
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:title, :description)
   end
 
 end
