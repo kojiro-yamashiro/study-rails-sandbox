129cb48a47bb588edd9c7bc62c63b6c29c4a1e18
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-03-18 00:14:56 +0900

    feat routesにdreamを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
+  get '/dreams', to: 'dreams#index'
+  get '/dreams/new', to: 'dreams#new'
+  post '/dreams', to: 'dreams#create'
+  get '/dream/:id', to: 'dreams#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
