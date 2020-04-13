d4efe14d2aff41e79d00a1041aead2c31a25a7ae
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-04-13 22:03:54 +0900

    add 編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
-  resources :dreams
-  # get '/dreams', to: 'dreams#index'
-  # get '/dreams/new', to: 'dreams#new'
-  # post '/dreams', to: 'dreams#create'
-  # get '/dreams/:id', to: 'dreams#show', as: 'show_dreams'
-  # get '/dreams/:id/edit', to: 'dreams#edit', as: 'edit_dreams'
-  # put '/dreams/:id', to: 'dreams#update', as: 'update_dreams'
+  # resources :dreams
+  get '/dreams', to: 'dreams#index'
+  get '/dreams/new', to: 'dreams#new'
+  post '/dreams', to: 'dreams#create'
+  get '/dreams/:id', to: 'dreams#show', as: 'show_dream'
+  get '/dreams/:id/edit', to: 'dreams#edit', as: 'edit_dream'
+  put '/dreams/:id', to: 'dreams#update', as: 'update_dream'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
