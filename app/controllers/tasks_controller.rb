2d0870d0790f11c18239f81ebda40bf797488d21
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-04-23 23:58:28 +0900

    add validation required name to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find_by(id: params[:id])
+    # @task = Task.find_by(id: params[:id])
   end
 
   def new
@@ -14,24 +14,32 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find_by(id: params[:id])
+    # @task = Task.find_by(id: params[:id])
   end
 
   def update
-    task = Task.find_by(id: params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク 「#{task.name}」を更新しました。"
+    # @task = Task.find_by(id: params[:id])
+
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク 「#{@task.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def create
-    task = Task.new(task_params)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def destroy
-    task = Task.find_by(id: params[:id])
-    task.destroy!
+    # @task = Task.find_by(id: params[:id])
+    @task.destroy!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
@@ -40,7 +48,7 @@ class TasksController < ApplicationController
   private
 
   def set_task
-    @task = Task.find(params[:id])
+    @task = Task.find_by(id: params[:id])
   end
 
   def task_params
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+
+  # 検証用の関数は、クラスメソッドであるvalidateメソッドにわたすことで検証時に利用することができる。
+  def validate_name_not_including_comma
+    errors.add(:name, "にカンマを含めることはできません。") if name&.include?(',')
+  end
 end
