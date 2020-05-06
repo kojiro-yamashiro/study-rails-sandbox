587f54ddb212f0c0b2ef726726c18add11af5bbb
kojiro-yamashiro <kojiro.yamashiro@gmail.com>
2020-05-07 00:05:09 +0900

    add comment out is uniqueness 理由はrails 6.1から暗黙的なチェックはパフォーマンスに知らずに影響を与えるためとのこと。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name , presence: true
-  validates :email , presence: true, uniqueness: true
+  
+  # uniquenessは着けない。https://github.com/rails/rails/pull/35350
+  validates :email , presence: true
+
 end
